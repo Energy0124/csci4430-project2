b268fbbc0c7959b297fb8839aafe25e4b42e68b8
Energy0124 <energystar.0124@gmail.com>
2017-04-22 22:51:46 +0800

    merge and fix format

--- nat.c
+++ nat.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <netinet/in.h>
-#include <linux/netfilter.h>  
+#include <linux/netfilter.h>
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <arpa/inet.h>	// for inet_pton()
-#include <errno.h>	// for "errno"
-#include <string.h>	// for strerror()
+#include <arpa/inet.h>    // for inet_pton()
+#include <errno.h>    // for "errno"
+#include <string.h>    // for strerror()
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <stdbool.h>
@@ -27,17 +27,15 @@
   (byte & 0x04 ? '1' : '0'), \
   (byte & 0x02 ? '1' : '0'), \
   (byte & 0x01 ? '1' : '0')
+
 //assumes little endian
-void printBits(size_t const size, void const * const ptr)
-{
-    unsigned char *b = (unsigned char*) ptr;
+void printBits(size_t const size, void const *const ptr) {
+    unsigned char *b = (unsigned char *) ptr;
     unsigned char byte;
     int i, j;
 
-    for (i=size-1;i>=0;i--)
-    {
-        for (j=7;j>=0;j--)
-        {
+    for (i = size - 1; i >= 0; i--) {
+        for (j = 7; j >= 0; j--) {
             byte = (b[i] >> j) & 1;
             printf("%u", byte);
         }
@@ -45,31 +43,35 @@ void printBits(size_t const size, void const * const ptr)
     puts("");
 }
 
-void print_mapping(char * o_IP,int o_port, char * s_IP, int s_port ){
-    printf("Original source IP address: %s\n",o_IP);
-    printf("Original source port: %d\n",o_port);
-    printf("Translated source IP address: %s\n",s_IP);
-    printf("Translated source port : %d\n",s_port);
+void print_mapping(in_addr_t o_IP, int o_port, char *s_IP, int s_port) {
+    struct in_addr o_ip_struct;
+    o_ip_struct.s_addr = o_IP;
+    printf("Original source IP address: %s\n", inet_ntoa(o_ip_struct));
+    printf("Original source port: %d\n", o_port);
+    printf("Translated source IP address: %s\n", s_IP);
+    printf("Translated source port : %d\n", s_port);
 }
+
 // Global variables
 struct sockaddr_in public_addr;
 struct sockaddr_in internal_addr;
-char * subnet_mask;
+char *subnet_mask;
 int original_port[2000]; // -1 is unused
-char * original_IP[2000];
+in_addr_t original_IP[2000];
+
 
 static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *msg,
                     struct nfq_data *pkt, void *data) {
     int i;
     unsigned int id = 0;
-    int shouldDrop=false;
+    int shouldDrop = false;
 
     struct nfqnl_msg_packet_hdr *header;
     struct nfqnl_msg_packet_hw *hwph;
-    int accept[20]= {1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1};
+    int accept[20] = {1, 1, 1, 1, 1,
+                      1, 1, 1, 1, 1,
+                      1, 1, 1, 1, 1,
+                      1, 1, 1, 1, 1};
 
     // print hw_protocol, hook and id
 
@@ -86,34 +88,34 @@ static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *msg,
     if (hwph) {
         int i, hlen = ntohs(hwph->hw_addrlen);
         printf("hw_src_addr=");
-        for (i = 0; i < hlen-1; i++)
+        for (i = 0; i < hlen - 1; i++)
             printf("%02x:", hwph->hw_addr[i]);
-        printf("%02x ", hwph->hw_addr[hlen-1]);
+        printf("%02x ", hwph->hw_addr[hlen - 1]);
     }
 
     // Print the payload;
 
     printf("\n[");
     unsigned char *pktData;
-    int len = nfq_get_payload(pkt, (char**)&pktData);
+    int len = nfq_get_payload(pkt, (char **) &pktData);
     if (len > 0) {
-        for (i=0; i<len; i++) {
+        for (i = 0; i < len; i++) {
             printf("%02x ", pktData[i]);
         }
     }
     printf("]\n");
 
     // Get IP header
-    struct iphdr *iph = (struct iphdr*) pktData;
+    struct iphdr *iph = (struct iphdr *) pktData;
 
-    char *saddr = inet_ntoa(*(struct in_addr *)&iph->saddr);
-    fprintf(stdout,"before source=%s; ",saddr);
+    char *saddr = inet_ntoa(*(struct in_addr *) &iph->saddr);
+    fprintf(stdout, "before source=%s; ", saddr);
 
-    char *daddr = inet_ntoa(*(struct in_addr *)&iph->daddr);
-    fprintf(stdout,"destination=%s}\n",daddr);
+    char *daddr = inet_ntoa(*(struct in_addr *) &iph->daddr);
+    fprintf(stdout, "destination=%s}\n", daddr);
 
     // Get TCP header
-    struct tcphdr *tcph = (struct tcphdr *) (((char*) iph)  + (iph->ihl << 2));
+    struct tcphdr *tcph = (struct tcphdr *) (((char *) iph) + (iph->ihl << 2));
 
 
     uint16_t sport, dport;           /* Source and destination ports */
@@ -123,41 +125,34 @@ static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *msg,
     int mask_int = atoi(subnet_mask);
     int local_mask = 0xffffffff << (32 - mask_int);
     printf("mask_int: \n");
-    printBits(sizeof(mask_int),&mask_int);
+    printBits(sizeof(mask_int), &mask_int);
 
     printf("local_mask: \n");
-    printBits(sizeof(local_mask),&local_mask);
+    printBits(sizeof(local_mask), &local_mask);
 
 
-
-    unsigned int local_network = ntohl(internal_addr.sin_addr.s_addr )& local_mask;
+    unsigned int local_network = ntohl(internal_addr.sin_addr.s_addr) & local_mask;
     printf("local_network: \n");
-    printBits(sizeof(local_network),&local_network);
+    printBits(sizeof(local_network), &local_network);
 //    char temp_str[INET_ADDRSTRLEN];
 //    inet_ntop(AF_INET, &(internal_addr.sin_addr), temp_str, INET_ADDRSTRLEN);
 //    printf("local network: %s\n",temp_str);
     sport = ntohs(tcph->source);
     dport = ntohs(tcph->dest);
     //change tcp header
-    iph->saddr= internal_addr.sin_addr.s_addr;
+//    iph->saddr= internal_addr.sin_addr.s_addr;
 //    tcph->source=htons(10000);//fix hard code source port
 
-    //recal tcp cksum
-    printf("ocheck: %d\n",iph->check);
-    tcph->check= tcp_checksum( pktData);
 
-    iph->check= ip_checksum( pktData);
-    struct iphdr *iph2 = (struct iphdr*) pktData;
-    printf("ncheck: %d\n",iph2->check);
 
-    int tempip=ntohl(iph->saddr);
+    int tempip = ntohl(iph->saddr);
     printf("source ip: \n");
-    printBits(sizeof(tempip),&tempip);
-    tempip=ntohl(iph->saddr) & local_mask;
+    printBits(sizeof(tempip), &tempip);
+    tempip = ntohl(iph->saddr) & local_mask;
     printf("source ip&local_mask: \n");
-    printBits(sizeof(tempip),&tempip);
+    printBits(sizeof(tempip), &tempip);
 
-    if ( (ntohl(iph->saddr) & local_mask) == local_network) {
+    if ((ntohl(iph->saddr) & local_mask) == local_network) {
 // outbound traffic
         printf("this is outbound\n");
 
@@ -168,55 +163,55 @@ static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *msg,
         int found = 0;
 
         // First check is the source IP already in NAT table
-        while(counter < 2000){
-            if(original_port[counter] == sport){
-                port = 10000 + counter ;
+        while (counter < 2000) {
+            if (original_port[counter] == sport) {
+                port = 10000 + counter;
                 found = 1;
                 break;
             }
 
-            counter ++;
+            counter++;
         }
 
         // If not, create entry and print map update
-        if(!found){
-            while(counter < 2000){
-                if(original_port[counter] == -1){
-                    port = 10000 + counter ;
+        if (!found) {
+            while (counter < 2000) {
+                if (original_port[counter] == -1) {
+                    port = 10000 + counter;
                     original_port[counter] = sport;
-                    original_IP[counter] = saddr;
-                    print_mapping(original_IP[counter],original_port[counter],daddr,dport);
+                    original_IP[counter] = iph->saddr;
+                    print_mapping(original_IP[counter], original_port[counter], daddr, dport);
                     break;
                 }
 
-                counter ++;
+                counter++;
             }
         }
 
         // replace source port of every outgoing datagram to NAT IP address, new port
 
         tcph->source = htons(port);
-        printf("port:%d dest:%d\n", ntohs(tcph->source),dport);
+        printf("port:%d dest:%d\n", ntohs(tcph->source), dport);
         iph->saddr = public_addr.sin_addr.s_addr;
 
-        saddr = inet_ntoa(*(struct in_addr *)&iph->saddr);
-        fprintf(stdout,"after change address=%s; ",saddr);
+        saddr = inet_ntoa(*(struct in_addr *) &iph->saddr);
+        fprintf(stdout, "after change address=%s; ", saddr);
     } else {
 // inbound traffic
         printf("this is inbound\n");
         int port = dport - 10000;
         // port exist
-        if( original_port[port] != -1){
+        if (original_port[port] != -1) {
             tcph->dest = htons(original_port[port]);
 
-        } else{
+        } else {
             printf("port is not in table\n");
         }
-        printf("port:%d dest:%d\n", sport,dport);
-        iph->daddr = internal_addr.sin_addr.s_addr;
+        printf("port:%d dest:%d\n", sport, dport);
+        iph->daddr = original_IP[port];
 
-        daddr = inet_ntoa(*(struct in_addr *)&iph->daddr);
-        fprintf(stdout,"after change address=%s; ",daddr);
+        daddr = inet_ntoa(*(struct in_addr *) &iph->daddr);
+        fprintf(stdout, "after change address=%s; ", daddr);
     }
 
     // TCP packets
@@ -230,12 +225,21 @@ static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *msg,
     // accept[id-1] = 1.
     // All packets with id > 20, will be accepted
     if (!shouldDrop) {
+        //recal tcp cksum
+        printf("ocheck: %d\n", iph->check);
+        tcph->check = 0;
+        iph->check = 0;
+
+        tcph->check = tcp_checksum(pktData);
+        iph->check = ip_checksum(pktData);
+        struct iphdr *iph2 = (struct iphdr *) pktData;
+        printf("ncheck: %d\n", iph2->check);
 
         printf("ACCEPT\n");
         return nfq_set_verdict(qh, id, NF_ACCEPT, len, pktData);
 
 
-    }else {
+    } else {
         printf("DROP\n");
         return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
     }
@@ -245,7 +249,7 @@ static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *msg,
 /*
  * Main program
  */
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     struct nfq_handle *h;
     struct nfq_q_handle *qh;
     struct nfnl_handle *nh;
@@ -253,17 +257,15 @@ int main(int argc, char **argv){
     int len;
     char buf[4096];
 
-    if(argc != 4){
+    if (argc != 4) {
         printf("Three argument expected.\n");
         exit(-1);
-    }
-    else
-    {
-        int  i;
-        for (  i = 0; i < 2000; i++ ) {
-            original_port[ i ] = -1; /* initialize to -1*/
-            original_IP[i] = -1;
-         }
+    } else {
+        int i;
+        for (i = 0; i < 2000; i++) {
+            original_port[i] = -1; /* initialize to -1*/
+            original_IP[i] = 0;
+        }
         if (inet_pton(AF_INET, (const char *) argv[1], &public_addr.sin_addr) == 0) {
             fprintf(stderr, "%s (line %d): %s - inet_pton():\n", __FILE__, __LINE__, __FUNCTION__);
             fprintf(stderr, "\tError message: Wrong public IP address format\n");
@@ -297,7 +299,7 @@ int main(int argc, char **argv){
         }
 
         // bind socket and install a callback on queue 0
-        if (!(qh = nfq_create_queue(h,  0, &Callback, NULL))) {
+        if (!(qh = nfq_create_queue(h, 0, &Callback, NULL))) {
             fprintf(stderr, "Error: nfq_create_queue()\n");
             exit(1);
         }
